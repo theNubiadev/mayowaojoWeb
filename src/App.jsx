c2e968985872122dae865197b4dbeec09024f374
theNubiadev <ajaiyeobajibola@gmail.com>
2025-07-03 19:00:32 +0100

    created the routes and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Route, Routes,  Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from "./pages/Home";
-import About from "./pages/About"
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
     <Router>
-      <div className="min-h-screen bg-white" >
+      <div className="min-h-screen bg-white">
         <Header />
         <main>
-        <Route  path="/" element={<Home />}/>
-        <Route  path="/about" element={<About />}/>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </>
-  )
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
