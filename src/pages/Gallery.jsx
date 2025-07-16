f5615144788fad31a31739a911c85237916d5586
theNubiadev <ajaiyeobajibola@gmail.com>
2025-07-16 17:54:24 +0100

    feat: Add lazy loading to images in Gallery component for improved performance

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -172,6 +172,7 @@ function Gallery() {
                       <img
                         src={item.image}
                         alt={item.title}
+                        loading='lazy'
                         className="w-full h-64 object-cover transition-transform duration-700 group-hover:scale-110"
                       />
                       <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
