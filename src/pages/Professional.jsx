d2f70fa45c9895d01b827a0742b42f7c121c9c93
theNubiadev <ajaiyeobajibola@gmail.com>
2025-07-24 17:09:46 +0100

    fix: Adjust padding in Professional page for improved layout

--- src/pages/Professional.jsx
+++ src/pages/Professional.jsx
@@ -63,7 +63,7 @@ function Professional() {
   ]
 
   return (
-    <div className="bg-white py-20 pt-4">
+    <div className="bg-white py-24 ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="text-center mb-16">
