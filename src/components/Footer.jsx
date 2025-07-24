9385a37b4b03d0fda3eb8e903325dc598f21e974
theNubiadev <ajaiyeobajibola@gmail.com>
2025-07-24 17:20:28 +0100

    fix: Update social media links and clean up Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {Mail, Phone, MapPin, Linkedin, Twitter, Facebook, Instagram} from "lucide-react";
+import { Mail, Phone, MapPin, Linkedin, Twitter, Facebook, Instagram } from "lucide-react";
 function Footer() {
   return (
- <footer className="bg-gray-900 text-white">
+    <footer className="bg-gray-900 text-white">
       <div className="max-w-6xl mx-auto px-6 lg:px-8 py-16">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Brand */}
@@ -12,18 +12,18 @@ function Footer() {
                 <span className="text-xl font-bold">Mayowa Ojo</span>
               </div>
             </div>
-          
+      
             <div className="flex space-x-4">
-              <a href="https://www.linkedln.com/mayowa-ojo-865b29bb" target="_blank" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
+              <a href="https://www.linkedin.com/in/mayowa-ojo-865b29bb?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
                 <Linkedin className="h-5 w-5" />
               </a>
-               <a href="https://www.instagram.com/mayowa_ojo1?utm_source=q&ish=aGZ2aW5IM3F0aDJ6"  target="_blank" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
+              <a href="https://www.instagram.com/mayowa_ojo1?utm_source=qr&igsh=aGZ2aW5lM3F0aDJ6" target="_blank" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
                 <Instagram className="h-5 w-5" />
               </a>
-              <a href="" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
+              {/* <a href="" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
                 <Twitter className="h-5 w-5" />
-              </a>
-                 <a href="https://www.facebook.com/share/1Aa3dyrj8U/" target="_blank" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
+              </a> */}
+              <a href="https://www.facebook.com/share/1Aa3dyrj8U/" target="_blank" className="w-10 h-10 bg-gray-800 rounded-lg flex items-center justify-center hover:bg-gray-700 transition-colors duration-200">
                 <Facebook className="h-5 w-5" />
               </a>
             </div>
